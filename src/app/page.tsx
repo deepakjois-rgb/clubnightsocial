447b83e0c99161323e55b5e4efc7e333fef8cc37
Deepak Jois <deepakjoismn@gmail.com>
2026-07-04 13:15:59 +0100

    flattened the root structure

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <main style={{
+      display: "flex",
+      height: "100vh",
+      alignItems: "center",
+      justifyContent: "center",
+      fontSize: "2rem",
+      fontWeight: "600"
+    }}>
+      Hello Club Night Social!
+    </main>
+  );
+}
